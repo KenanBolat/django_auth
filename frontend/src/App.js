8bca67bff7ce561003a7d57d41b7d8f2d3b95042
Kenan Bolat <kenan.bolat@yandex.com.tr>
2023-10-15 22:03:01 +0300

    Add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header';
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
           <div>
+            <Header>Home</Header>
             <Route exact path='/' component={HomePage} />
             <Route path='/login' component={LoginPage} />
           </div>
